989307f894839ff8e30b7b6da92237968292b108
LeynerTG <lstrianag@udistrital.edu.co>
2024-10-03 23:17:09 -0500

    Add files via upload

---
+++ CORTE1/CALCULADORA/main.py
@@ -0,0 +1,11 @@
+import clase
+
+calculadora = clase.Calculadora()
+
+# Llamada a cada método, los cuales devuelven el resultado de la operación
+suma = calculadora.sumar()
+resta = calculadora.restar()
+multiplicacion = calculadora.multiplicar()
+division = calculadora.dividir()
+
+
